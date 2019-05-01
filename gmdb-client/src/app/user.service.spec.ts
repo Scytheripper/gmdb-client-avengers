475bb480131d4b280cd6ebf0fa07336f9280f5ec
tikatedipika <tikate.dipika@gmail.com>
2019-05-01 13:19:02 -0400

    added user service with login, update user and tests

---
+++ gmdb-client/src/app/user.service.spec.ts
@@ -0,0 +1,69 @@
+import { TestBed, getTestBed, inject } from '@angular/core/testing';
+
+import { UserService } from './user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+let injector: TestBed;
+let httpMock: HttpTestingController;
+
+describe('UserService', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientModule,
+      HttpClientTestingModule
+    ]
+  }));
+
+  beforeEach( () => {
+    injector = getTestBed();
+    httpMock = injector.get(HttpTestingController);
+  });
+
+  afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    httpMock.verify();
+  }));
+
+  it('should be created', () => {
+    const service: UserService = TestBed.get(UserService);
+    expect(service).toBeTruthy();
+  });
+
+  it('should get user with given username and password', () => {
+    //Setup
+    const service: UserService = TestBed.get(UserService);
+    const user = {id: 1, username: 'guest', password: 'password', email: 'email@email.com'};
+
+
+    service.login('guest', 'password').subscribe((user) => {
+      expect(user.username).toEqual('guest');
+    });
+
+    const req = httpMock.expectOne('http://localhost:4200/assets/users.json');
+    expect(req.request.method).toBe('GET');
+    req.flush(user);
+  });
+
+  it('should set the currently logged in user', () => {
+    //Setup 
+    const service: UserService = TestBed.get(UserService);
+    const user = {id: 1, username: 'guest', password: 'password', email: 'email@email.com'};
+
+    //Exercise
+    service.setLoggedInUser(user);
+
+    //Assert
+    expect(service.loggedInUser).toEqual(user);
+  });
+
+  it('should modify the user', () => {
+    const service: UserService = TestBed.get(UserService);
+    const user = {id: 1, username: 'guest', password: 'password', email: 'email@email.com'};
+
+    service.updateUser(user);
+
+    const req = httpMock.expectOne('http://localhost:4200/assets/users.json');
+    expect(req.request.method).toBe('PUT');
+  });
+
+
+});
---
+++ gmdb-client/src/app/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { User } from './user';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  loggedInUser: User;
+
+  constructor(private http: HttpClient) { }
+
+  login(username,password):Observable<User> {
+    return this.http.get<User>('http://localhost:4200/assets/users.json');
+  }
+
+  setLoggedInUser(user: User) {
+    this.loggedInUser = user;
+  }
+
+  getLoggedInUser() {
+    return this.loggedInUser;
+  }
+
+  updateUser(user: User) {
+    this.http.put('http://localhost:4200/assets/users.json', user).subscribe();
+  }
+}
