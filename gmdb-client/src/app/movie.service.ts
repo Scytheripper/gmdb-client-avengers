5b006b6e68ca32b26b2c4aa9bffde28343a6ac46
training <a>
2019-05-01 13:42:28 -0400

    TDD

--- gmdb-client/src/app/movie.service.ts
+++ gmdb-client/src/app/movie.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Movie } from './movie.js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
 
-  getMovies(){
-
+  
+  constructor(private httpclient: HttpClient) { 
+    
   }
 
-  getMoviesByName(){
-//TODO method
+  getMovies(): Observable<Movie[]>{
+    return this.httpclient.get<Movie[]>('http://localhost:4200/assets/movies-data.json');
+  }
+
+  getMoviesByTitle() {
+    //TODO method
   }
 }
