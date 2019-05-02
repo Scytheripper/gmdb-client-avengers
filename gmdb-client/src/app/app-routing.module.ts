f7bcbed2b10f10cd44ab70487ae2bce4d5fc23ba
training <a>
2019-05-02 12:04:18 -0400

    resolve merge conflict

--- gmdb-client/src/app/app-routing.module.ts
+++ gmdb-client/src/app/app-routing.module.ts
@@ -8,12 +8,8 @@ import { MovieDetailsComponent } from './movie-details/movie-details.component';
 const routes: Routes = [
   {path: 'accountDetails', component: AccountDetailComponent},
   {path: 'home',component:HomeComponent},
-<<<<<<< HEAD
-  {path: 'movieDetails',component:MovieDetailsComponent}
-=======
   {path: 'movieDetails',component:MovieDetailsComponent},
   {path: 'signup', component: SignupComponent},
->>>>>>> d4a8bf52bee735eeb3ccffd14df6a498e3269bc8
 ];
 
 @NgModule({
