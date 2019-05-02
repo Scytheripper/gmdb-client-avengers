225213ce672446b21f628f55a473f87c8fec120e
Sanjay Shivraj <shivraj.sanjay@gmail.com>
2019-05-01 20:25:38 -0400

    added template files for the sign up comp

--- gmdb-client/src/app/app.module.ts
+++ gmdb-client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
